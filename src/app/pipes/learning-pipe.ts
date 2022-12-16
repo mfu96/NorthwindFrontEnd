1594d1170821e8b64aa60480886fbb822853601f
mfu96 <furkan42uslu@gmail.com>
2022-12-16 16:57:38 +0300

    161222

---
+++ src/app/pipes/learning-pipe.ts
@@ -0,0 +1,21 @@
+//Pipe sana gelen bir veriyi farklı şekilde yansıtma işine yarar
+//Örneğin küçük gelen bir yazıyı büyütebilirsin
+
+//https://angular.io/api/common/CommonModule --Bilgi
+
+//Angular'a tanımlı hazır (built-in) pipler
+
+//async          Observable (nesneyi) ekrana basar 
+//json           Veriyi json şekilinde (Postaman'deki gibi) okunabilir şekilde verir
+//uppercase      Verinin tüm harflerini büyülterek ekrana basar
+//lowercase      Verinin tüm harflerini küçülterek ekrana basar
+//titlecase      Verinin ilk harflerini büyülterek ekrana basar
+//slice          Veriyi bölerek senin istediğin değeri verir
+                //slice ör   {{"Bilgisayar" | slice:3:9}} => gisaya
+//date:'hh:mm'  Gelen saati veridğin değere göre ekrana basar
+//currnecy      Verinin para birimi olarak gösterilmesini sağlar
+                //currnecy:'₺' gibi default $ yerine istediğin birimi basabilirsin
+//percent       //verinin yüzde biçiminde gösterilmesini sağlar
+//decimal       //veriyi ondalıklı olarak gösterir
+//keyvalue      Object ve map'lerin ngFor ile dönmesini sağlar ???               
+
